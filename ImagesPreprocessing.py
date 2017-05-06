e36f9d973c933ccdaf48d81ebe9742c2409e3cc7
AmemiyaYuko <kyd_dong@hotmail.com>
2017-05-05 20:01:57 -0400

    Improved extractor

--- ImagesPreprocessing.py
+++ ImagesPreprocessing.py
@@ -2,6 +2,10 @@ import os
 import cv2
 import numpy as np
 import argparse
+import random
+import sys
+import pickle
+import struct
 
 
 def read_gnt(file):
@@ -9,38 +13,73 @@ def read_gnt(file):
         header = np.fromfile(file, dtype="uint8", count=10)
         if not header.size: break
         sample_size = header[0] + (header[1] << 8) + (header[2] << 16) + (header[3] << 24)
-        tag_code = header[4] + (header[5] << 8)
+        tag_code = header[5] + (header[4] << 8)
         width = header[6] + (header[7] << 8)
         height = header[8] + (header[9] << 8)
         image = np.fromfile(file, dtype='uint8', count=width * height).reshape((height, width))
         yield image, tag_code
 
 
+def getChineseCharacter(tag_code):
+    return np.unicode(tag_code, 'gb2312')
+
+
 def file_list(gnt_dir):
     return [os.path.join(gnt_dir, file_name) for file_name in os.listdir(gnt_dir)]
 
 
+def getDict(dir):
+    files=file_list(dir)
+    char_set = set()
+    for file in files:
+        f=open(file,'r')
+        for _, tag_code in read_gnt(f):
+            uni = struct.pack('>H', tag_code).decode('gb2312')
+            char_set.add(uni)
+    char_list = list(char_set)
+    cdict = dict(zip(sorted(char_list), range(len(char_list))))
+    pickle.dump(cdict, open('char_dict.txt', 'wb'))
+    return cdict
+
+
 def arg_parser():
-    parser=argparse.ArgumentParser()
-    parser.add_argument("--data-dir",type=str,help="Absolute path of gnt dateset",default=os.path.abspath("E:\HWDB1.1trn_gnt"))
-    parser.add_argument("--output-dir",type=str,help="Absolute path for storing pictures",default=os.path.abspath("E:\HWDB_Dataset"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gnt-dir", type=str, help="Absolute path of gnt dateset",
+                        default=os.path.abspath("E:/Datasets/HWDB/sub/gnt"))
+    parser.add_argument("--output-dir", type=str, help="Absolute path for storing pictures",
+                        default=os.path.abspath("E:/Datasets/HWDB/sub"))
+    parser.add_argument("--dict",type=bool,default=True)
     return parser.parse_args()
 
 
-def main(args):
-    files = [open(file_path, "r") for file_path in
-             file_list(args.data_dir)]
-    i = 0
-    for file in files:
-        for (image, tag_code) in (read_gnt(file)):
-            i += 1
-            if os.path.isdir(os.path.join(args.output_dir, str(tag_code))):
+def extractor(in_dir,out_dir,char_dict):
+    i=0
+    for file_name in file_list(in_dir):
+        f=open(file_name,"r")
+        for image,tag_code in read_gnt(f):
+            i+=1
+            tag_code_uni=struct.pack('>H', tag_code).decode('gb2312')
+            tag_str=out_dir+"/"+'%0.5d'%char_dict[tag_code_uni]
+            if os.path.isdir(tag_str):
                 pass
             else:
-                os.mkdir(os.path.join(args.output_dir, str(tag_code)))
-            cv2.imwrite(os.path.join(args.output_dir, str(tag_code)) + "\\" + str(i) + ".jpg", img=image)
+                os.mkdir(tag_str)
+            cv2.imwrite(tag_str+'/'+str(i)+".png",image)
+        f.close()
+
+def main(args):
+    trn_path=os.path.join(args.gnt_dir,"trn")
+    tst_path=os.path.join(args.gnt_dir,"tst")
+    trn_out_path=os.path.join(args.output_dir,"trn")
+    tst_out_path = os.path.join(args.output_dir, "tst")
+    char_dict=getDict(trn_path)
+    extractor(trn_path,trn_out_path,char_dict)
+    extractor(tst_path,tst_out_path, char_dict)
 
 
-if __name__=='__main__':
+
+
+
+
+if __name__ == '__main__':
     main(arg_parser())
-
